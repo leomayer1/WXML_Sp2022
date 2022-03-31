29918d18fbb65391e8e103ab51dfd517f5214d06
leopoldmayer <leomayer@uw.edu>
2022-03-31 09:30:34 -0700

    Added some more comments

--- src/ideal.lean
+++ src/ideal.lean
@@ -5,7 +5,10 @@ section defs
 /- Let R be an arbitrary commutative ring -/
 variables (R : Type*) [comm_ring R]
 
-/- Definition of an ideal -/
+/- Definition of an ideal.
+  In order to define an ideal, one needs to specify its
+  underlying set (carrier), as well as three proofs
+-/
 @[ext] structure ideal :=
 (carrier : set R)
 (zero_mem' : (0 : R) ∈ carrier)
@@ -15,9 +18,13 @@ variables (R : Type*) [comm_ring R]
 /- The set {0} is an ideal -/
 instance : has_zero (ideal R) := ⟨{
   carrier := {0}, --the singleton set containing 0
-  zero_mem' := rfl,
-  add_mem' := λ x y hx hy, by simp * at *,
-  smul_mem' := λ r x hx, by simp * at *
+  zero_mem' := rfl, --proof that 0 ∈ {0}
+  add_mem' := λ x y hx hy, by simp * at *, --proof that if x ∈ {0} and y ∈ {0}, then
+                                          -- x + y ∈ {0}. "simp" is a tactic that is
+                                          -- able to perform basic simplifications,
+                                          -- so it can replace the hypothesis x ∈ {0}
+                                          -- with x = 0, and it knows 0 + 0 = 0.
+  smul_mem' := λ r x hx, by simp * at *   --simp also knows r*0 = 0
 }⟩
 
 /- R itself is an ideal-/
@@ -34,13 +41,13 @@ def prin {R : Type*} [comm_ring R] (x : R) : ideal R := {
   zero_mem' := ⟨0, (zero_mul x).symm⟩,
   add_mem' :=
     begin
-      rintros _ _ ⟨a, rfl⟩ ⟨b, rfl⟩,
-      exact ⟨a + b, (add_mul _ _ _).symm⟩
+      rintros _ _ ⟨a, rfl⟩ ⟨b, rfl⟩, --if a*x, b*x ∈ prin x...
+      exact ⟨a + b, (add_mul _ _ _).symm⟩ --then a*x + b*x = (a + b)*x ∈ prin x
     end,
   smul_mem' :=
     begin
-      rintros r _ ⟨a, rfl⟩,
-      exact ⟨r * a, (mul_assoc _ _ _).symm⟩
+      rintros r _ ⟨a, rfl⟩, -- if a*x ∈ prin x...
+      exact ⟨r * a, (mul_assoc _ _ _).symm⟩ -- then r*(a*x) = (r*a)*x ∈ prin x
     end
 }
 
@@ -48,6 +55,9 @@ end defs
 
 /- Here we define the membership relation, as well as any
   operations on ideals: intersection, sum, etc.
+  We could also add a lot from e.g. Atiyah MacDonald ch 1:
+  ideal quotients, radical ideals, the radical operation,
+  and facts about these objects.
 -/
 section operations
 variables {R : Type*} [comm_ring R]
@@ -91,7 +101,7 @@ instance : has_add (ideal R) := ⟨λ I J,
 
 end operations
 
-lemma mem_prin {R : Type*} [comm_ring R] (x : R) : x ∈ prin x := ⟨1, by ring⟩
+lemma mem_prin {R : Type*} [comm_ring R] (x : R) : x ∈ prin x := ⟨1, by simp⟩
 
 
 /- Definitions and facts about prime and maximal ideals -/
@@ -99,7 +109,10 @@ variables {R : Type*} [comm_ring R]
 def prime (I : ideal R) := ∀ x y : R, x*y ∈ I → x ∈ I ∨ y ∈ I
 def maximal (I : ideal R) := ∀ J : ideal R, I ⊂ J → J = univ R
 
-/- Pretty messy, definitely could use more outside lemmas -/
+/- Pretty messy, definitely could use more outside lemmas
+  This is what formalizing proofs "usually" looks like,
+  with the entire thing written in tactic mode
+-/
 theorem maximal_implies_prime {I : ideal R} (hI : maximal I) : prime I :=
 begin
   intros x y hxy,
