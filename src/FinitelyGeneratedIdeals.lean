a0d49ff5b88204ea3a19d01660c8a6fdc4a7b519
Kevin Kuei <you@example.com>
2022-05-18 16:33:36 -0700

    Finished

--- src/FinitelyGeneratedIdeals.lean
+++ src/FinitelyGeneratedIdeals.lean
@@ -1,19 +1,15 @@
 import ideal
 import data.finset.basic
 import tactic
-import group_theory.group_action.big_operators
+import group_theory.group_action.basic
 
 
---open_locale big_operators
+open_locale big_operators
 
 variables {R : Type} [comm_ring R]
 variable S : finset R
---variable f : S → R
---#check ∑ a : S, ((f a) * a : R)
    
-instance tada : distrib_mul_action R R :=
-{ smul_add := mul_add,
-  smul_zero := mul_zero }  
+ 
 
 def ideal_gen_by  : ideal R := {
   carrier := {x : R | ∃ I : S → R, x = ∑ a : S, ((I a) * a : R)  }, 
@@ -23,8 +19,6 @@ def ideal_gen_by  : ideal R := {
     simp only [finset.univ_eq_attach, set.mem_set_of_eq],
     use 0,
     simp only [pi.zero_apply, zero_mul, finset.sum_const_zero],
-    
-    
   end,
   add_mem' :=
     begin
@@ -34,7 +28,6 @@ def ideal_gen_by  : ideal R := {
       simp [hx, hy],
       ring,
       rw finset.sum_add_distrib,
-
     end,
   smul_mem' :=
     begin
@@ -43,17 +36,10 @@ def ideal_gen_by  : ideal R := {
       let rf : S → R := λ s, r*f(s),
       use rf,
       rw hx,
-      --ring,
       rw [←smul_eq_mul, finset.smul_sum],
-
-      
-      
+      apply finset.sum_congr rfl,
+      intros a ha,
+      rw [smul_eq_mul, mul_assoc],    
     end
 }
 
-
-example (f : S → R) (r : R) : r * ∑ a : S, f a = ∑ a : S, (r * f a) :=
-begin
-  suggest,
-end
-#check finset.smul_sum
